0daed8fdbb7e7dbf0da84420ea88e8c8631faf81
Yash Harfode <120653482+yashharfode@users.noreply.github.com>
2026-04-19 08:21:52 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Ab values sirf environment variables se aayengi
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBHs5NvvXRwoY7wUAeZ3t6As7rBzQmp2aE",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "connection-crm.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "connection-crm",
-  storageBucket:
-    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "connection-crm.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "561033372566",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:561033372566:web:6f09526269fa9eba4533b7",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-PD4VHETK9M",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
 let analytics: ReturnType<typeof getAnalytics> | null = null;
+
+// Window check to prevent errors during SSR (Server Side Rendering)
 if (typeof window !== "undefined") {
   analytics = getAnalytics(app);
 }
